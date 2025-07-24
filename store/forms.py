9b551e9fa16abe5fce21a465ac44b361fcf0ecef
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2025-07-24 15:49:01 +0530

    final check

--- store/forms.py
+++ store/forms.py
@@ -3,22 +3,26 @@ from django import forms
 from store.models import *
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
-from django import forms 
+from django import forms
 from django.forms import modelformset_factory
 from store.models import Product
 
 # Register user form with user type selection
+
+
 class RegisterForm(forms.ModelForm):
     USER_TYPE_CHOICES = (
         ('customer', 'Register as Customer'),
         ('supplier', 'Register as Supplier'),
     )
-    
+
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
-    user_type = forms.ChoiceField(choices=USER_TYPE_CHOICES, widget=forms.RadioSelect)
+    user_type = forms.ChoiceField(
+        choices=USER_TYPE_CHOICES, widget=forms.RadioSelect)
     phone = forms.CharField(max_length=15, required=False)
-    address = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), required=False)
+    address = forms.CharField(widget=forms.Textarea(
+        attrs={'rows': 3}), required=False)
 
     class Meta:
         model = User
@@ -31,7 +35,7 @@ class RegisterForm(forms.ModelForm):
 
         if password != confirm_password:
             raise forms.ValidationError("Passwords do not match.")
-        
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password'])
@@ -45,26 +49,31 @@ class RegisterForm(forms.ModelForm):
                 address=self.cleaned_data.get('address', '')
             )
         return user
-        
+
 # Login form
+
+
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label='Username', max_length=254)
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    
+
     class Meta:
         model = User
         fields = ['username', 'password']
 
-#Product Form
+# Product Form
+
+
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'price', 'description', 'category', 'subcategory', 'stock','slug']
+        fields = ['name', 'price', 'description',
+                  'category', 'subcategory', 'stock', 'slug']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4}),
             'price': forms.NumberInput(attrs={'step': '0.01'}),
         }
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['slug'].disabled = True
@@ -76,31 +85,38 @@ class ProductForm(forms.ModelForm):
         if 'category' in self.data:
             try:
                 category_id = int(self.data.get('category'))
-                self.fields['subcategory'].queryset = SubCategory.objects.filter(category_id=category_id).order_by('name')
+                self.fields['subcategory'].queryset = SubCategory.objects.filter(
+                    category_id=category_id).order_by('name')
             except (ValueError, TypeError):
                 pass
         elif self.instance.pk:
-            self.fields['subcategory'].queryset = self.instance.category.subcategories.order_by('name')
+            self.fields['subcategory'].queryset = self.instance.category.subcategories.order_by(
+                'name')
+
 
 ProductImageFormSet = modelformset_factory(
     ProductImage,
     fields=('image',),
-    extra=1,  
+    extra=1,
     max_num=5,
     can_delete=True
 )
 
 # Category Form
+
+
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields = ['name', 'image','slug']
+        fields = ['name', 'image', 'slug']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['slug'].disabled = True
 
 # SubCategory Form
+
+
 class SubCategoryForm(forms.ModelForm):
     class Meta:
         model = SubCategory
